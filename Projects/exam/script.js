eb7fd38b8b57e6ccb535572a23c7ba062a60cb56
Dominic Walter <59534138+DominicwalterLOF@users.noreply.github.com>
2021-10-17 13:22:23 +0530

    added logics

--- Projects/exam/script.js
+++ Projects/exam/script.js
@@ -31,11 +31,12 @@ questions = {
     3:["The filament of a light bulb has surface area 64 mm2. The filament can be considered as a blackbody at temperature 2500 K emitting radiation like a point source when viewed from far. At nightthe light bulb is observed from a distance of 100 m. Assume the pupil of the eyes of the observer tobe circular with radius 3 mm. Then(Take Stefan-Boltzmann constant = 5.67 × 10−8 Wm−2K−4, Wien’s displacement constant =2.90 × 10−3 m-K, Planck’s constant = 6.63 × 10−34 Js, speed of light in vacuum = 3.00 ×108 ms−1)", "power radiated by the filament is in the range 642 W to 645 W", "radiated power entering into one eye of the observer is in the range 3.15 × 10−8 W to 3.25 × 10−8 W", "the wavelength corresponding to the maximum intensity of light is 1160 nm", "taking the average wavelength of emitted radiation to be 1740 nm, the total number of photons entering per second into one eye of the observer is in the range 2.75 × 1011 to 2.85 × 1011"]
 };
 
-currentQn = "qb1";
+currentQn = "1";
+currentObj = "qb1";
 
 for(i=1; i <= Object.keys(questions).length; i++){
     addQnButton(i);
-    responses["qb" + i] = "&|&";
+    responses[i] = "&|&";
 }
 
 var li = document.getElementsByClassName("li1");
@@ -45,26 +46,51 @@ for(var i = 0;i<li.length;i++){
 }
 
 function panbutclick(e){
-    x = e.target.attributes.id.value;
-    if(responses[x] == "&|&"){
-        unanswered(x);
-        responses[x] = "|&|"
+    currentObj = e.target.attributes.id.value;
+    currentQn = currentObj.split("b")[1];
+    if(responses[currentQn] == "&|&"){
+        unanswered(currentObj);
     }
-    currentQn = x.innerText;
-    renderQuestion(currentQn);
-    return currentQn;       
+    
+    renderQuestion();    
 }
 
-function renderQuestion(q){
-    document.getElementById("QuesNo").innerHTML = "Question: " + q;
-    document.getElementById("QuesMain").innerHTML = questions[q][0];
-    x = renderOptions(q);
+function renderQuestion(){
+    console.log(currentQn);
+    document.getElementById("QuesNo").innerHTML = "Question: " + currentQn;
+    document.getElementById("QuesMain").innerHTML = questions[currentQn][0];
+    x = renderOptions(currentObj);
+    renderResponse();
 }
 
 function renderOptions(q){
+    q = q.split("b")[1];
+    document.getElementById("1option").innerHTML = questions[q][1];
+    document.getElementById("2option").innerHTML = questions[q][2];
+    document.getElementById("3option").innerHTML = questions[q][3];
+    document.getElementById("4option").innerHTML = questions[q][4];
 
 }
 
 function checkAnswered(){
 
 }
+
+function optSel(optionID){
+    responses[currentQn] = optionID;
+    answered(currentObj);
+}
+
+function renderResponse(){
+
+    if(responses[currentQn] == "&|&"){
+        for(i = 1; i < 5; i++){
+            document.getElementById("option" + i).checked = false;
+        }
+    }
+    else{
+        document.getElementById("option" + responses[currentQn]).checked = true;
+    }
+}
+
+document.getElementById("qb1").click();
