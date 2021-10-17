db5ab7d620cf1da5cae329ca341ee758399b7f2e
Dominic Walter <59534138+DominicwalterLOF@users.noreply.github.com>
2021-10-17 10:37:31 +0530

    exam update

--- Projects/exam/script.js
+++ Projects/exam/script.js
@@ -24,9 +24,16 @@ function unanswered(qn){
 
 responses = {};
 
+questions = {
+
+    1:["who is the chief executive officer of LOF ?", "Dominic", "Walter", "Dominic Walter", "Dominic Walter T"],
+    2:["Expansion of WWW :", "World ww", "World wide w", "World wide web"],
+    3:["The filament of a light bulb has surface area 64 mm2. The filament can be considered as a blackbody at temperature 2500 K emitting radiation like a point source when viewed from far. At nightthe light bulb is observed from a distance of 100 m. Assume the pupil of the eyes of the observer tobe circular with radius 3 mm. Then(Take Stefan-Boltzmann constant = 5.67 × 10−8 Wm−2K−4, Wien’s displacement constant =2.90 × 10−3 m-K, Planck’s constant = 6.63 × 10−34 Js, speed of light in vacuum = 3.00 ×108 ms−1)", "power radiated by the filament is in the range 642 W to 645 W", "radiated power entering into one eye of the observer is in the range 3.15 × 10−8 W to 3.25 × 10−8 W", "the wavelength corresponding to the maximum intensity of light is 1160 nm", "taking the average wavelength of emitted radiation to be 1740 nm, the total number of photons entering per second into one eye of the observer is in the range 2.75 × 1011 to 2.85 × 1011"]
+};
+
 currentQn = "qb1";
 
-for(i=1; i <= 20; i++){
+for(i=1; i <= Object.keys(questions).length; i++){
     addQnButton(i);
     responses["qb" + i] = "&|&";
 }
@@ -43,17 +50,21 @@ function panbutclick(e){
         unanswered(x);
         responses[x] = "|&|"
     }
-    currentQn = x;
-    renderQuestion(x);
-    return x;       
+    currentQn = x.innerText;
+    renderQuestion(currentQn);
+    return currentQn;       
 }
 
 function renderQuestion(q){
-    document.getElementById("").innerHTML = "";
+    document.getElementById("QuesNo").innerHTML = "Question: " + q;
+    document.getElementById("QuesMain").innerHTML = questions[q][0];
     x = renderOptions(q);
-    document.getElementById("").innerHTML = "";
 }
 
 function renderOptions(q){
+
+}
+
+function checkAnswered(){
 
 }
